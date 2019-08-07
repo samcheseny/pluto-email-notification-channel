140d762ddf40f629b15795ef5ab4f4104d2970e8
Samuel <ndarasam@gmail.com>
2019-08-07 12:38:20 +0300

    Add config for colors for each log level

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,13 +1,14 @@
 /**
  * Patterns to be found in the log templates
  */
-exports.LOG_PATTERNS = {
+exports.LOG_TEMPLATE_PATTERNS = {
   MESSAGE: '{{LOG_MESSAGE}}',
   SERVER: '{{LOG_SERVER}}',
   PID: '{{LOG_PID}}',
   FILENAME: '{{LOG_FILENAME}}',
   LINE: '{{LOG_LINE}}',
-  TIMESTAMP: '{{LOG_TIMESTAMP}}'
+  TIMESTAMP: '{{LOG_TIMESTAMP}}',
+  COLOR: '{{LOG_COLOR}}'
 }
 
 /**
@@ -36,3 +37,14 @@ exports.REQUIRED_ENV_PROPERTIES = [
   'MAIL_SENDER',
   'MAIL_RECIPIENTS'
 ]
+
+/**
+ * Color codes for each log level
+ */
+exports.SEVERITY_COLOR_CODE = {
+  emergency: '#dc3545',
+  error: '#28a745',
+  warning: '#ffc107',
+  info: '#4fc08d',
+  debug: '#6c757d'
+}
--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { LOG_PATTERNS } = require('../configs')
+const { LOG_TEMPLATE_PATTERNS, SEVERITY_COLOR_CODE } = require('../configs')
 
 class Utilities {
   /**
@@ -9,16 +9,19 @@ class Utilities {
   static getFormattedHtmlEmailBody (loggedMessage) {
     try {
       let template = fs
-        .readFileSync(`../../templates/${loggedMessage.severity}.html`)
+        .readFileSync(`../../templates/log-email.html`)
         .toString()
 
+      let logColor = SEVERITY_COLOR_CODE[loggedMessage.severity]
+
       return template
-        .replace(LOG_PATTERNS.MESSAGE, loggedMessage.message)
-        .replace(LOG_PATTERNS.SERVER, loggedMessage.serverName)
-        .replace(LOG_PATTERNS.PID, loggedMessage.PID)
-        .replace(LOG_PATTERNS.FILENAME, loggedMessage.filename)
-        .replace(LOG_PATTERNS.LINE, loggedMessage.line)
-        .replace(LOG_PATTERNS.TIMESTAMP, loggedMessage.timestamp)
+        .replace(LOG_TEMPLATE_PATTERNS.MESSAGE, loggedMessage.message)
+        .replace(LOG_TEMPLATE_PATTERNS.SERVER, loggedMessage.serverName)
+        .replace(LOG_TEMPLATE_PATTERNS.PID, loggedMessage.PID)
+        .replace(LOG_TEMPLATE_PATTERNS.FILENAME, loggedMessage.filename)
+        .replace(LOG_TEMPLATE_PATTERNS.LINE, loggedMessage.line)
+        .replace(LOG_TEMPLATE_PATTERNS.TIMESTAMP, loggedMessage.timestamp)
+        .replace(LOG_TEMPLATE_PATTERNS.COLOR, logColor)
     } catch (error) {
       return loggedMessage.message
     }
